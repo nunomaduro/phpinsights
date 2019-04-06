d6e3553b8a8497b2374ae050274cd7536c6c16dc
Nuno Maduro <enunomaduro@gmail.com>
2019-04-06 22:05:10 +0200

    wip

---
+++ stubs/phpinsights.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Preset
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default preset that will be used by PHP Insights
+    | to make your code reliable, simple, and clean. However, you can always
+    | adjust the `Metrics` and `Insights` below in this configuration file.
+    |
+    | Supported: "default", "laravel", "symfony"
+    |
+    */
+
+    'preset' => 'default',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Metrics
+    |--------------------------------------------------------------------------
+    |
+    | Here you may adjust all the various `Metrics` that will be used by
+    | PHP Insights. .................................................
+    | ............................................................
+    |
+    */
+
+    'metrics' => [
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Insights
+    |--------------------------------------------------------------------------
+    |
+    | Here you may adjust all the various `Insights` that will be used by
+    | PHP Insights. .................................................
+    | ............................................................
+    |
+    */
+
+    'insights' => [
+    ],
+
+];
