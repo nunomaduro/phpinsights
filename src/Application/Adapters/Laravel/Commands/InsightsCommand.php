acc5865060b2a411c29b9f0c4fbfbff0bac3071a
Nuno Maduro <enunomaduro@gmail.com>
2019-05-01 17:35:54 +0200

    wip

--- src/Application/Adapters/Laravel/Commands/InsightsCommand.php
+++ src/Application/Adapters/Laravel/Commands/InsightsCommand.php
@@ -31,7 +31,7 @@ final class InsightsCommand extends Command
         Kernel::bootstrap();
 
         if (! file_exists($this->input->getOption('config-path'))) {
-            $this->output->note('Consider publish the configuration using the: php artisan vendor:publish nunomaduro/phpinsights.');
+            $this->output->note('Consider publish the configuration using the: "php artisan vendor:publish "NunoMaduro\PhpInsights\Application\Adapters\Laravel\InsightsServiceProvider"');
         }
 
         $analyseCommand->__invoke($this->input, $this->output);
