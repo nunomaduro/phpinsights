0bbcf0c87ef51fb0fc3a7fa29952ab42eb875177
Nuno Maduro <enunomaduro@gmail.com>
2019-05-19 13:06:31 +0200

    Adds website

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'PHP Insigths',
+  description: 'The perfect starting point to analyze the code quality of your PHP projects',
+  sidebar: true,
+  themeConfig: {
+    sidebar: [
+      '/get-started',
+      '/configuration',
+      '/contribute',
+      '/support'
+    ]
+  }
+}
