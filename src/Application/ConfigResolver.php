923e2b54404315ef36b4566f96c67698ab254687
Oliver Nybroe <olivernybroe@gmail.com>
2019-05-29 17:15:26 +0200

    Fix: Change merge priority

--- src/Application/ConfigResolver.php
+++ src/Application/ConfigResolver.php
@@ -41,7 +41,7 @@ final class ConfigResolver
 
         foreach (self::$presets as $presetClass) {
             if ($presetClass::getName() === $preset && is_array($config)) {
-                $config = array_merge_recursive($presetClass::get(), $config);
+                $config = array_merge_recursive($config, $presetClass::get());
             }
         }
 
