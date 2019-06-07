5afb4f7e477301825bb175d019e08eb2a38d535f
Nuno Maduro <enunomaduro@gmail.com>
2019-06-07 13:00:37 +0200

    Adds more tweets

--- docs/.vuepress/components/Testimonial.vue
+++ docs/.vuepress/components/Testimonial.vue
@@ -12,6 +12,28 @@
             </div>
 
             <div class="column">
+                <blockquote class="twitter-tweet" data-link-color="#3eaf7c" data-lang="en"><p lang="en" dir="ltr">
+                Improving code with <a href="https://twitter.com/hashtag/phpinsights?src=hash&amp;ref_src=twsrc%5Etfw">
+                #phpinsights</a> by <a href="https://twitter.com/enunomaduro?ref_src=twsrc%5Etfw">@enunomaduro</a>
+                <br>Great package to write better code. <a href="https://t.co/J4QaaPRK4x">pic.twitter.com/J4QaaPRK4x</a>
+                </p>&mdash; Marc Mascort Bou (@marcmascort) <a href="https://twitter.com/marcmascort/status/1135937826061791234?ref_src=twsrc%5Etfw">
+                June 4, 2019</a></blockquote>
+            </div>
+
+            <div class="column">
+                <blockquote class="twitter-tweet" data-link-color="#3eaf7c" data-lang="en"><p lang="en" dir="ltr">Shoutout
+                to <a href="https://twitter.com/enunomaduro?ref_src=twsrc%5Etfw">@enunomaduro</a> for this great package
+                <a href="https://twitter.com/hashtag/phpinsights?src=hash&amp;ref_src=twsrc%5Etfw">#phpinsights</a>
+                <br>Spent a couple of hours fixing the little details for a better sexier code !!<br>Really great package!
+                <a href="https://t.co/UXSBHQ5TTY">pic.twitter.com/UXSBHQ5TTY</a></p>&mdash; Elie Andraos (@andzilla31)
+                <a href="https://twitter.com/andzilla31/status/1136250113079144450?ref_src=twsrc%5Etfw">June 5, 2019</a></blockquote>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="column">
                 <blockquote class="twitter-tweet" data-link-color="#3eaf7c"><p lang="en" dir="ltr">After every refactor or
                     bug fix<br>Two things are necessary.<br><br>1️⃣ Run the tests<br><br>2️⃣ Checking code quality through
                     php insights by <a href="https://twitter.com/enunomaduro?ref_src=twsrc%5Etfw">@enunomaduro</a>
@@ -20,9 +42,16 @@
                             href="https://twitter.com/Sarthaksavvy/status/1131219636781903872?ref_src=twsrc%5Etfw">May 22,
                         2019</a></blockquote>
             </div>
-        </div>
 
-        <div class="row">
+            <div class="column">
+                <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">My app seems good.<br>Thanks
+                to <a href="https://twitter.com/enunomaduro?ref_src=twsrc%5Etfw">@enunomaduro</a>
+                for such wonderfull package.<a href="https://twitter.com/hashtag/phpinsights?src=hash&amp;ref_src=twsrc%5Etfw">#phpinsights</a>
+                <a href="https://t.co/pbSurXyt2d">pic.twitter.com/pbSurXyt2d</a></p>&mdash; Indra 印洞羅 (@neindk)
+                <a href="https://twitter.com/neindk/status/1130706349745090560?ref_src=twsrc%5Etfw">May 21, 2019
+                </a></blockquote>
+            </div>
+
             <div class="column">
                 <blockquote class="twitter-tweet" data-link-color="#3eaf7c"><p lang="en" dir="ltr">I&#39;m very inspired to
                     improve the overall code quality of my projects after analyzing the code with the <a
@@ -33,6 +62,9 @@
                         27, 2019</a></blockquote>
             </div>
 
+        </div>
+
+        <div class="row">
             <div class="column">
                 <blockquote class="twitter-tweet" data-link-color="#3eaf7c"><p lang="en" dir="ltr">After two full
                     days &quot;playing around&quot; with <a
@@ -43,6 +75,26 @@
                             href="https://twitter.com/dsampaolo/status/1131516809591611392?ref_src=twsrc%5Etfw">May
                         23, 2019</a></blockquote>
             </div>
+
+            <div class="column">
+                <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">My first run of
+                <a href="https://twitter.com/hashtag/phpinsights?src=hash&amp;ref_src=twsrc%5Etfw">#phpinsights</a>
+                on one of my projects near completion. Amazing package <a href="https://twitter.com/enunomaduro?ref_src=twsrc%5Etfw">
+                @enunomaduro</a>. So many helpful insights. A must for all future Laravel projects. And you find out that you are not
+                good enough too 🤓 <a href="https://t.co/hgIZGMV5Kl">pic.twitter.com/hgIZGMV5Kl</a></p>&mdash; Sardar Nawab Khan (@SNKhan300)
+                <a href="https://twitter.com/SNKhan300/status/1128687946872303621?ref_src=twsrc%5Etfw">May 15, 2019</a></blockquote>
+            </div>
+
+            <div class="column">
+                <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I was curious if I can use
+                <a href="https://twitter.com/hashtag/phpinsights?src=hash&amp;ref_src=twsrc%5Etfw">#phpinsights</a>
+                to analyze himself. <br>Of course, that is possible! <a href="https://t.co/iGf65H50yA">
+                pic.twitter.com/iGf65H50yA</a></p>&mdash; Vidal (@vidalvas)
+                <a href="https://twitter.com/vidalvas/status/1128391464566886401?ref_src=twsrc%5Etfw">
+                May 14, 2019</a></blockquote>
+            </div>
+
+
         </div>
     </div>
 </template>
