e04b2045216ecb12e754e6ed31a784483cda43fe
Jibé Barth <barth.jib@gmail.com>
2019-06-08 18:48:06 +0200

    Add tests for ComposerFinder and ComposerLoader

---
+++ tests/Domain/ComposerFinderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain;
+
+use NunoMaduro\PhpInsights\Domain\Collector;
+use NunoMaduro\PhpInsights\Domain\ComposerFinder;
+use NunoMaduro\PhpInsights\Domain\Exceptions\ComposerNotFound;
+use PHPUnit\Framework\TestCase;
+
+final class ComposerFinderTest extends TestCase
+{
+    public function testGetComposerPath(): void
+    {
+        $collector = new Collector(__DIR__ . '/Insights/Composer/Fixtures/Valid');
+        $path = ComposerFinder::getPath($collector);
+
+        self::assertEquals(
+            __DIR__ . '/Insights/Composer/Fixtures/Valid/composer.json',
+            $path
+        );
+    }
+
+    public function testGetComposerPathThrowExceptionIfNoComposerJsonExist(): void
+    {
+        self::expectException(ComposerNotFound::class);
+
+        $collector = new Collector(__DIR__ . '/Insights/Composer/Fixtures');
+        ComposerFinder::getPath($collector);
+    }
+}
