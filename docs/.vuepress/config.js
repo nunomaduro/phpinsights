479f9bfb6b95d8ccc0d613a28fb3568c1668248f
Jibé Barth <barth.jib@gmail.com>
2019-06-09 19:36:43 +0200

    Add style documentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,7 @@ module.exports = {
           '/insights/code',
           '/insights/architecture',
           '/insights/complexity',
+          '/insights/style',
         ]
       },
       '/contribute',
