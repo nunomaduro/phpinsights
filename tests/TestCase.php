d1355d773849af9872f54f9f2ed84646412605d3
Jibé Barth <barth.jib@gmail.com>
2019-10-26 20:52:31 +0200

    Set concrete configuration class in testCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,8 @@ namespace Tests;
 
 use NunoMaduro\PhpInsights\Application\ConfigResolver;
 use NunoMaduro\PhpInsights\Domain\Analyser as DomainAnalyser;
+use NunoMaduro\PhpInsights\Domain\Configuration;
+use NunoMaduro\PhpInsights\Domain\Container;
 use NunoMaduro\PhpInsights\Domain\Insights\InsightCollection;
 use NunoMaduro\PhpInsights\Domain\Insights\InsightCollectionFactory;
 use NunoMaduro\PhpInsights\Domain\MetricsFinder;
@@ -77,6 +79,12 @@ abstract class TestCase extends BaseTestCase
         string $dir = ''
     ) : InsightCollection {
         $config = ConfigResolver::resolve($config, $dir);
+
+        $container = Container::make();
+        \assert($container instanceof \League\Container\Container);
+
+        $configurationDefinition = $container->extend(Configuration::class);
+        $configurationDefinition->setConcrete($config);
 
         $analyser = new DomainAnalyser();
 
