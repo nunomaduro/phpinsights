a81ee3b18a524afafffa682121c1a019a2738c3c
Jibé Barth <barth.jib@gmail.com>
2019-11-17 17:56:39 +0100

    Add test to reproduce issue 318

---
+++ tests/Domain/ConfigurationTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain;
+
+use NunoMaduro\PhpInsights\Domain\Configuration;
+use NunoMaduro\PhpInsights\Domain\Exceptions\InvalidConfiguration;
+use NunoMaduro\PhpInsights\Domain\LinkFormatter\FileLinkFormatter;
+use NunoMaduro\PhpInsights\Domain\LinkFormatter\NullFileLinkFormatter;
+use PHPUnit\Framework\TestCase;
+
+final class ConfigurationTest extends TestCase
+{
+    public function testPassEmptyArrayReturnDefaultConfiguration(): void
+    {
+        $configuration = new Configuration([]);
+
+        self::assertEquals(getcwd(), $configuration->getDirectory());
+        self::assertEquals('default', $configuration->getPreset());
+        self::assertEquals([], $configuration->getAdd());
+        self::assertEquals([], $configuration->getExcludes());
+        self::assertEquals([], $configuration->getConfig());
+        self::assertEquals([], $configuration->getRemoves());
+    }
+
+    public function testWithNullIde(): void
+    {
+        $config = [
+            'ide' => null,
+        ];
+
+        $configuration = new Configuration($config);
+
+        self::assertInstanceOf(NullFileLinkFormatter::class, $configuration->getFileLinkFormatter());
+    }
+
+    public function testWithEmptyIde(): void
+    {
+        $config = [
+            'ide' => '',
+        ];
+
+        $configuration = new Configuration($config);
+
+        self::assertInstanceOf(NullFileLinkFormatter::class, $configuration->getFileLinkFormatter());
+    }
+
+    public function testWithSelectedIde(): void
+    {
+        $config = [
+            'ide' => 'sublime',
+        ];
+
+        $configuration = new Configuration($config);
+        self::assertInstanceOf(FileLinkFormatter::class, $configuration->getFileLinkFormatter());
+    }
+
+    public function testWithUnknowIde(): void
+    {
+        self::expectException(InvalidConfiguration::class);
+        self::expectExceptionMessage('Unknow IDE "notepad++"');
+
+        $config = [
+            'ide' => 'notepad++',
+        ];
+
+        new Configuration($config);
+    }
+}
