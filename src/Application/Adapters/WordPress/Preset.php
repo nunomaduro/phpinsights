66c234c07b960952d20e8177a4e8882ad7d332dd
Denis Žoljom <denis.zoljom@gmail.com>
2020-03-12 17:06:58 +0100

    Add WordPress adapter

---
+++ src/Application/Adapters/WordPress/Preset.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NunoMaduro\PhpInsights\Application\Adapters\Symfony;
+
+use NunoMaduro\PhpInsights\Application\Composer;
+use NunoMaduro\PhpInsights\Application\ConfigResolver;
+use NunoMaduro\PhpInsights\Application\DefaultPreset;
+use NunoMaduro\PhpInsights\Domain\Contracts\Preset as PresetContract;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
+
+/**
+ * @internal
+ */
+final class Preset implements PresetContract
+{
+    public static function getName(): string
+    {
+        return 'wordpress';
+    }
+
+    public static function get(Composer $composer): array
+    {
+        $config = [
+            'exclude' => [
+                'web/wp',
+                'web/.htaccess',
+                'web/app/mu-plugins/',
+                'web/app/upgrade',
+                'web/app/uploads/',
+                'web/app/plugins/',
+            ],
+            'config' => [
+                ForbiddenFunctionsSniff::class => [
+                    'forbiddenFunctions' => [
+                        'eval' => null,
+                        'error_log' => null,
+                        'print_r' => null,
+                    ],
+                ],
+            ],
+        ];
+
+        return ConfigResolver::mergeConfig(DefaultPreset::get($composer), $config);
+    }
+
+    public static function shouldBeApplied(Composer $composer): bool
+    {
+        $requirements = $composer->getRequirements();
+
+        foreach (array_keys($requirements) as $requirement) {
+            $requirement = (string) $requirement;
+
+            if (strpos($requirement, 'johnpbloch/wordpress') !== false
+                || strpos($requirement, 'roots/wordpress') !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
