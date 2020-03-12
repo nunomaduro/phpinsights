219bec8ab9df1a44b64b94c5f2fb48dea4cfb869
Denis Žoljom <denis.zoljom@gmail.com>
2020-03-12 18:04:34 +0100

    Fix adapter namespace and add WordPress to ConfigResolver

--- src/Application/Adapters/WordPress/Preset.php
+++ src/Application/Adapters/WordPress/Preset.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace NunoMaduro\PhpInsights\Application\Adapters\Symfony;
+namespace NunoMaduro\PhpInsights\Application\Adapters\WordPress;
 
 use NunoMaduro\PhpInsights\Application\Composer;
 use NunoMaduro\PhpInsights\Application\ConfigResolver;
--- src/Application/ConfigResolver.php
+++ src/Application/ConfigResolver.php
@@ -8,6 +8,7 @@ use NunoMaduro\PhpInsights\Application\Adapters\Drupal\Preset as DrupalPreset;
 use NunoMaduro\PhpInsights\Application\Adapters\Laravel\Preset as LaravelPreset;
 use NunoMaduro\PhpInsights\Application\Adapters\Magento2\Preset as Magento2Preset;
 use NunoMaduro\PhpInsights\Application\Adapters\Symfony\Preset as SymfonyPreset;
+use NunoMaduro\PhpInsights\Application\Adapters\WordPress\Preset as WordPressPreset;
 use NunoMaduro\PhpInsights\Application\Adapters\Yii\Preset as YiiPreset;
 use NunoMaduro\PhpInsights\Domain\Configuration;
 use NunoMaduro\PhpInsights\Domain\Contracts\Preset;
@@ -32,6 +33,7 @@ final class ConfigResolver
         SymfonyPreset::class,
         YiiPreset::class,
         Magento2Preset::class,
+        WordPressPreset::class,
         DefaultPreset::class,
     ];
 
