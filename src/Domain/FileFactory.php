59bc196ab6874a44aaea8e31f0ef5496d915e6f9
Oliver Nybroe <olivernybroe@gmail.com>
2020-04-03 09:47:17 +0200

    feat(ruleset): Only instantiate ruleset once

--- src/Domain/FileFactory.php
+++ src/Domain/FileFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace NunoMaduro\PhpInsights\Domain;
 
+use PHP_CodeSniffer\Config;
+use PHP_CodeSniffer\Ruleset;
 use RuntimeException;
 use Symfony\Component\Finder\SplFileInfo;
 
@@ -12,6 +14,23 @@ use Symfony\Component\Finder\SplFileInfo;
  */
 final class FileFactory
 {
+    /** @var \PHP_CodeSniffer\Ruleset */
+    private $ruleset;
+
+    /** @var \PHP_CodeSniffer\Config */
+    private $config;
+
+    public function __construct()
+    {
+        $config = new Config([], false);
+        $config->__set('tabWidth', 4);
+        $config->__set('annotations', false);
+        $config->__set('encoding', 'UTF-8');
+
+        $this->config = $config;
+        $this->ruleset = new Ruleset($config);
+    }
+
     public function createFromFileInfo(SplFileInfo $smartFileInfo): File
     {
         $path = $smartFileInfo->getRealPath();
@@ -24,7 +43,9 @@ final class FileFactory
 
         return new File(
             $path,
-            $smartFileInfo->getContents()
+            $smartFileInfo->getContents(),
+            $this->config,
+            $this->ruleset
         );
     }
 }
