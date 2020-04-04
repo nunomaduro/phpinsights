3bf565a1fb277a31da90137dc8eff702f7030161
Jibé Barth <barth.jib@gmail.com>
2020-04-04 12:19:48 +0200

    Always use php-parallel-lint

--- src/Domain/Insights/Syntax.php
+++ src/Domain/Insights/Syntax.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace NunoMaduro\PhpInsights\Domain\Insights;
 
-use NunoMaduro\PhpInsights\Application\Console\Formatters\PathShortener;
 use NunoMaduro\PhpInsights\Domain\Contracts\GlobalInsight;
 use NunoMaduro\PhpInsights\Domain\Contracts\HasDetails;
 use NunoMaduro\PhpInsights\Domain\Details;
@@ -37,59 +36,6 @@ final class Syntax extends Insight implements HasDetails, GlobalInsight
     }
 
     public function process(): void
-    {
-        if (class_exists(\JakubOnderka\PhpParallelLint\Application::class) &&
-            file_exists(getcwd() . '/vendor/bin/parallel-lint')) {
-            $this->processParallelLint();
-
-            return;
-        }
-
-        $this->processLint();
-    }
-
-    private function processLint(): void
-    {
-        $cmdLine = '';
-        $phpPath = (string) Config::getExecutablePath('php');
-        $isAnalyseDir = is_dir($this->collector->getCommonPath());
-
-        foreach ($this->collector->getFiles() as $filename) {
-            if ($this->shouldSkipFile($filename)) {
-                continue;
-            }
-            if ($isAnalyseDir === true) {
-                $filename = $this->collector->getCommonPath() . DIRECTORY_SEPARATOR . $filename;
-            }
-            $cmdLine .= sprintf(
-                '%s -l -d display_errors=1 -d error_prepend_string="" %s;',
-                escapeshellcmd($phpPath),
-                escapeshellarg($filename)
-            );
-        }
-
-        $process = Process::fromShellCommandline($cmdLine);
-        $process->run();
-        $errors = explode(PHP_EOL, $process->getErrorOutput());
-
-        foreach ($errors as $error) {
-            if (preg_match('/^.*error:(.*) in (.*) on line ([\d]+)/m', trim($error), $matches) === 1) {
-                $filename = $matches[2];
-                if ($isAnalyseDir === true) {
-                    $filename = str_replace($this->collector->getCommonPath() . DIRECTORY_SEPARATOR, '', $filename);
-                } elseif ($filename === $this->collector->getCommonPath()) {
-                    $filename = basename($filename);
-                }
-
-                $this->details[] = Details::make()
-                    ->setMessage('PHP syntax error: ' . trim($matches[1]))
-                    ->setFile($filename)
-                    ->setLine((int) $matches[3]);
-            }
-        }
-    }
-
-    private function processParallelLint(): void
     {
         $phpPath = (string) Config::getExecutablePath('php');
         $filesToAnalyse = array_map(static function (string $file): string {
