3e249c58af968e5cb1d21273e89c6b40bef19e94
Oliver Nybroe <olivernybroe@gmail.com>
2020-04-10 17:13:01 +0200

    test(rector): Fix an issue with fixtures

--- tests/Fixtures/Domain/Fixer/UnorderedUse.php
+++ tests/Fixtures/Domain/Fixer/UnorderedUse.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Fixtures\Domain\Fixer;
 
-use NunoMaduro\PhpInsights\Application\ConfigResolver;
 use NunoMaduro\PhpInsights\Domain\Analyser;
+use NunoMaduro\PhpInsights\Application\ConfigResolver;
 
 abstract class UnorderedUse
 {
