2bfc91b16882b85314c4b0874bcc170e28db37f0
Jibé Barth <barth.jib@gmail.com>
2020-05-30 12:47:02 +0200

    Handle cache in runner

--- src/Domain/Runner.php
+++ src/Domain/Runner.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace NunoMaduro\PhpInsights\Domain;
 
+use NunoMaduro\PhpInsights\Domain\Contracts\DetailsCarrier;
 use NunoMaduro\PhpInsights\Domain\Contracts\FileProcessor as FileProcessorContract;
 use NunoMaduro\PhpInsights\Domain\Contracts\GlobalInsight;
 use NunoMaduro\PhpInsights\Domain\Contracts\Repositories\FilesRepository;
+use Psr\SimpleCache\CacheInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\SplFileInfo;
@@ -30,8 +32,15 @@ final class Runner
 
     private FilesRepository $filesRepository;
 
+    private CacheInterface $cache;
+
+    private string $cacheKey;
+
     /** @var array<\NunoMaduro\PhpInsights\Domain\Contracts\GlobalInsight|\NunoMaduro\PhpInsights\Domain\Contracts\Insight> */
     private array $globalInsights = [];
+
+    /** @var array<\NunoMaduro\PhpInsights\Domain\Contracts\Insight> */
+    private array $allInsights = [];
 
     public function __construct(OutputInterface $output, FilesRepository $filesRepository)
     {
@@ -41,6 +50,8 @@ final class Runner
         $container = Container::make();
 
         $this->filesProcessors = $container->get(FileProcessorContract::FILE_PROCESSOR_TAG);
+        $this->cache = $container->get(CacheInterface::class);
+        $this->cacheKey = $container->get(Configuration::class)->getCacheKey();
     }
 
     /**
@@ -48,6 +59,7 @@ final class Runner
      */
     public function addInsights(array $insights): void
     {
+        $this->allInsights = array_merge($insights, $this->allInsights);
         foreach ($insights as $insight) {
             if ($insight instanceof GlobalInsight) {
                 $this->globalInsights[] = $insight;
@@ -113,10 +125,50 @@ final class Runner
 
     private function processFile(SplFileInfo $file): void
     {
+        $cacheKey = 'insights.' . $this->cacheKey . '.' . md5($file->getContents());
+        if ($this->cache->has($cacheKey)) {
+            $detailsByInsights = $this->cache->get($cacheKey);
+            /** @var \NunoMaduro\PhpInsights\Domain\Contracts\Insight $insight */
+            foreach ($this->allInsights as $insight) {
+                if (! $insight instanceof DetailsCarrier || ! isset($detailsByInsights[$insight->getInsightClass()])) {
+                    continue;
+                }
+
+                array_walk(
+                    $detailsByInsights[$insight->getInsightClass()],
+                    static function (Details $details) use ($insight): void {
+                        $insight->addDetails($details);
+                    }
+                );
+            }
+
+            return;
+        }
+
         /** @var FileProcessorContract $fileProcessor */
         foreach ($this->filesProcessors as $fileProcessor) {
             $fileProcessor->processFile($file);
         }
+
+        $this->cacheDetailsForFile($cacheKey, $file);
+    }
+
+    private function cacheDetailsForFile(string $cacheKey, SplFileInfo $file): void
+    {
+        $detailsByInsights = [];
+        /** @var \NunoMaduro\PhpInsights\Domain\Contracts\Insight $insight */
+        foreach ($this->allInsights as $insight) {
+            if (! $insight instanceof DetailsCarrier || ! $insight->hasIssue()) {
+                continue;
+            }
+            $details = array_filter(
+                $insight->getDetails(),
+                static fn (Details $detail): bool => $detail->getFile() === $file->getRealPath()
+            );
+            $detailsByInsights[$insight->getInsightClass()] = $details;
+        }
+
+        $this->cache->set($cacheKey, $detailsByInsights);
     }
 
     private function createProgressBar(int $max = 0): ProgressBar
