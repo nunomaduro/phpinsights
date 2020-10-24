d458a70969d9d2cd77f0f81e17ec78915d2a0f23
Jibé Barth <barth.jib@gmail.com>
2020-10-24 20:11:21 +0200

    Locker construct differ between v1 and v2 for composer

--- src/Domain/ComposerLoader.php
+++ src/Domain/ComposerLoader.php
@@ -9,6 +9,7 @@ use Composer\Factory;
 use Composer\IO\NullIO;
 use Composer\Json\JsonFile;
 use Composer\Package\Locker;
+use Composer\Repository\RepositoryManager;
 
 /**
  * @internal
@@ -28,20 +29,32 @@ final class ComposerLoader
             $io = new NullIO();
             $composerPath = ComposerFinder::getPath($collector);
             self::$composer = (new Factory())->createComposer($io, ComposerFinder::getPath($collector), false, null, false);
-
-            $lockFile = pathinfo($composerPath, PATHINFO_EXTENSION) === 'json'
-                ? substr($composerPath, 0, -4).'lock'
-                : $composerPath . '.lock';
-
-            $composerContent = file_get_contents($composerPath);
-            if ($composerContent === false) {
-                throw new \InvalidArgumentException('Unable to get content of ' . $composerPath);
-            }
-
-            $locker = new Locker($io, new JsonFile($lockFile, null, $io), self::$composer->getRepositoryManager(), self::$composer->getInstallationManager(), $composerContent);
-            self::$composer->setLocker($locker);
+            self::$composer->setLocker(self::getLocker($composerPath, $io));
         }
 
         return self::$composer;
+    }
+
+    private static function getLocker(string $composerPath, NullIO $io): Locker
+    {
+        if (self::$composer === null) {
+            throw new \RuntimeException('Cannot get locker until composer is not initialized');
+        }
+        $lockFile = pathinfo($composerPath, PATHINFO_EXTENSION) === 'json'
+            ? substr($composerPath, 0, -4).'lock'
+            : $composerPath . '.lock';
+
+        $composerContent = file_get_contents($composerPath);
+        if ($composerContent === false) {
+            throw new \InvalidArgumentException('Unable to get content of ' . $composerPath);
+        }
+
+        $lockerConstructor = new \ReflectionMethod(Locker::class, '__construct');
+
+        // In composer v2, RepositoryManager is no longer needed
+        /** @phpstan-ignore-next-line */
+        return $lockerConstructor->getParameters()[2]->getType()->getName() === RepositoryManager::class
+            ? new Locker($io, new JsonFile($lockFile, null, $io), self::$composer->getRepositoryManager(), self::$composer->getInstallationManager(), $composerContent)
+            : new Locker($io, new JsonFile($lockFile, null, $io), self::$composer->getInstallationManager(), $composerContent); /** @phpstan-ignore-line */
     }
 }
