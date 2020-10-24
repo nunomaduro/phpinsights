b864c3efff35cd7e5f55589d75108249571cc718
Jibé Barth <barth.jib@gmail.com>
2020-10-24 16:48:39 +0200

    Avoid to instanciate whole container in construct

--- tests/Application/Console/Formatters/FormatResolverTest.php
+++ tests/Application/Console/Formatters/FormatResolverTest.php
@@ -14,7 +14,6 @@ use NunoMaduro\PhpInsights\Application\Console\Formatters\Json;
 use NunoMaduro\PhpInsights\Application\Console\Formatters\Multiple;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
