eed341bb15a516efa8e031e4fabc1fe69d19bdd8
Jibé Barth <barth.jib@gmail.com>
2021-02-09 18:55:35 +0100

    Feature/php8 and GitHub action (#448)
    * Fix syntax check analysis only one file
    
    * Add 8.0 php version support
    
    * Init workflows
    
    * Fix uninitialized properties when we just launch fix
    
    * Disable CI error when analyse only changed files, it's global analyse that handle error
    
    * Clean travis to keep only docker process
    
    * Build docker image
    
    * Delete travis file
    
    * Set docker repository name in env vars
    
    * Upgrade dockerfile as it's now tested by PR
    
    * Fix phpstan errors
    
    * Enable phpunit tests on PHP8
    
    * Migrate format of phpunit.xml
    
    * Fix some infinite loop with PHP8 when parsing not valid php file

--- tests/Domain/Insights/InvalidPhpCodeTest.php
+++ tests/Domain/Insights/InvalidPhpCodeTest.php
@@ -8,29 +8,21 @@ use Tests\TestCase;
 
 final class InvalidPhpCodeTest extends TestCase
 {
-    /**
-     * @return array<string, array<string>>
-     */
-    public static function invalidCodeProvider(): array
-    {
-        return [
-            'Semi colon after extend class' => [
-                __DIR__ . '/Fixtures/InvalidPhpCode/SemiColonAfterExtendClass.php',
-            ],
-            'Unclosed comment' => [
-                __DIR__ . '/Fixtures/InvalidPhpCode/UnclosedComment.php',
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider invalidCodeProvider
-     */
-    public function testNotFailingOnSemiColonAfterExtendClass(string $path): void
+    public function testNotFailingOnSemiColonAfterExtendClass(): void
     {
         $this->runAnalyserOnPreset(
             'default',
-            [$path]
+            [__DIR__ . '/Fixtures/InvalidPhpCode/SemiColonAfterExtendClass.php']
+        );
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testNotFailingOnUnclosedComment(): void
+    {
+        $this->runAnalyserOnPreset(
+            'default',
+            [__DIR__ . '/Fixtures/InvalidPhpCode/UnclosedComment.php']
         );
 
         $this->expectNotToPerformAssertions();
