f4c77697f0812ea4ec839600ec2ae7251544322a
50bhan <Sobhan.Sharifi.Sadeghi@gmail.com>
2021-04-21 17:08:40 +0430

    Revert ForbiddenSecurityIssues check

--- src/Domain/Insights/ForbiddenSecurityIssues.php
+++ src/Domain/Insights/ForbiddenSecurityIssues.php
@@ -80,6 +80,10 @@ final class ForbiddenSecurityIssues extends Insight implements HasDetails, Globa
             array_map(static fn (array $detail): string => $detail['version'], $packages['packages'])
         );
 
+        if ($packagesToCheck === false) {
+            $packagesToCheck = [];
+        }
+
         try {
             $advisoryList = $this->retrieveAdvisoriesListForPackages(array_keys($packagesToCheck));
         } catch (Throwable $e) {
