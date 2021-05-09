429e73a7be9723e08738f05c77da35be7c7666ff
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-05-09 21:52:20 +0100

    Set working directory to fix --exclude args
    The call to parallel-lint would always use the common path in tests and
    any other time the common path differed from `getcwd()`. This meant any
    --exclude args in `$cmdLine` would require fully resolved paths, which
    would likely only end up with similar "arg list too long" issues.
    
    Instead, this sets the working directory of the process when it doesn't
    match the common path (and it's not a single file being analysed).
    Behaviour is the same, but now parallel-lint can resolve the excludes.

--- src/Domain/Insights/SyntaxCheck.php
+++ src/Domain/Insights/SyntaxCheck.php
@@ -38,34 +38,20 @@ final class SyntaxCheck extends Insight implements HasDetails, GlobalInsight
 
     public function process(): void
     {
-        $phpPath = (string) Config::getExecutablePath('php');
-
-        $binary = sprintf(
-            '%s %s',
-            escapeshellcmd($phpPath),
-            escapeshellarg($this->vendorParentPathFind() . '/vendor/bin/parallel-lint')
-        );
-        if (DIRECTORY_SEPARATOR === '\\') {
-            $binary = $this->vendorParentPathFind() . '\vendor\bin\parallel-lint.bat';
-        }
-
-        $toAnalyse = '.';
-        if (\count($this->collector->getFiles()) === 1) {
-            // We ask to analyse one file, just proceed it
-            $files = $this->collector->getFiles();
-            $toAnalyse = \array_pop($files);
-        } elseif (rtrim($this->collector->getCommonPath(), DIRECTORY_SEPARATOR) !== getcwd()) {
-            $toAnalyse = $this->collector->getCommonPath();
-        }
+        $toAnalyse = $this->getTarget();
 
         $cmdLine = sprintf(
             '%s --no-colors --no-progress --json %s %s',
-            $binary,
+            $this->getBinary(),
             implode(' ', $this->getShellExcludeArgs()),
             $toAnalyse
         );
 
         $process = Process::fromShellCommandline($cmdLine);
+
+        if ($toAnalyse === '.' && getcwd() !== rtrim($this->collector->getCommonPath(), DIRECTORY_SEPARATOR)) {
+            $process->setWorkingDirectory($this->collector->getCommonPath());
+        }
         $process->run();
 
         $output = json_decode($process->getOutput(), true, 512, JSON_THROW_ON_ERROR);
@@ -81,6 +67,21 @@ final class SyntaxCheck extends Insight implements HasDetails, GlobalInsight
         }
     }
 
+    private function getBinary(): string
+    {
+        $parentPath = $this->vendorParentPathFind();
+
+        if (DIRECTORY_SEPARATOR === '\\') {
+            return $parentPath . '\vendor\bin\parallel-lint.bat';
+        }
+
+        return sprintf(
+            '%s %s',
+            escapeshellcmd((string) Config::getExecutablePath('php')),
+            escapeshellarg($parentPath . '/vendor/bin/parallel-lint')
+        );
+    }
+
     /**
      * @return array<string>
      */
@@ -94,6 +95,17 @@ final class SyntaxCheck extends Insight implements HasDetails, GlobalInsight
             static fn (string $file): string => '--exclude ' . escapeshellarg($file),
             array_merge($rootExcludes, $this->excludedFiles, LocalFilesRepository::DEFAULT_EXCLUDE)
         );
+    }
+
+    private function getTarget(): string
+    {
+        $files = $this->collector->getFiles();
+
+        if (count($files) === 1) {
+            return \array_pop($files);
+        }
+
+        return '.';
     }
 
     private function vendorParentPathFind(): string
