cdcbd0baf1e6bfb2ce92881105a2c30589a47aa4
Gerry Demaret <gerry@tigron.be>
2021-06-15 16:39:23 +0800

    Feature: support configuring fixer indentation
    This adds support for controlling the WhitespacesFixerConfig for fixers
    implementing WhitespacesAwareFixerInterface.

--- src/Domain/InsightLoader/FixerLoader.php
+++ src/Domain/InsightLoader/FixerLoader.php
@@ -9,7 +9,9 @@ use NunoMaduro\PhpInsights\Domain\Contracts\Insight;
 use NunoMaduro\PhpInsights\Domain\Contracts\InsightLoader;
 use NunoMaduro\PhpInsights\Domain\Insights\FixerDecorator;
 use PhpCsFixer\Fixer\ConfigurableFixerInterface;
+use PhpCsFixer\Fixer\WhitespacesAwareFixerInterface;
 use PhpCsFixer\Fixer\FixerInterface;
+use PhpCsFixer\WhitespacesFixerConfig;
 
 /**
  * @internal
@@ -34,6 +36,15 @@ final class FixerLoader implements InsightLoader
             /** @var array<string> $excludeConfig */
             $excludeConfig = $config['exclude'];
             unset($config['exclude']);
+        }
+
+        if (isset($config['indent'])) {
+            if ($fixer instanceof WhitespacesAwareFixerInterface) {
+                $fixerConfig = new WhitespacesFixerConfig($config['indent']);
+                $fixer->setWhitespacesConfig($fixerConfig);
+            }
+
+            unset($config['indent']);
         }
 
         if ($fixer instanceof ConfigurableFixerInterface && count($config) > 0) {
