63e9d3ff43f2e26b57b380212503d0ebe19ce489
Danny Herpol <15183537+hdcore@users.noreply.github.com>
2023-09-28 05:39:07 +0200

    PHP 8.2 fix for dynamic property $exclude (#653)
    * PHP 8.2 fix for dynamic property $exclude
    
    * Add phpdoc value type for iterable type array
    
    * formatting
    
    ---------
    
    Co-authored-by: Danny Herpol <hdcore@lachjekrom.com>
    Co-authored-by: Chris Gmyr <cmgmyr@gmail.com>

--- src/Domain/Insights/SniffDecorator.php
+++ src/Domain/Insights/SniffDecorator.php
@@ -34,13 +34,18 @@ final class SniffDecorator implements Sniff, Insight, DetailsCarrier, Fixable
      */
     private array $excludedFiles;
 
-    public function __construct(Sniff $sniff, string $dir)
+    /**
+     * SniffDecorator constructor.
+     *
+     * @param array<string> $exclude
+     */
+    public function __construct(Sniff $sniff, string $dir, array $exclude)
     {
         $this->sniff = $sniff;
         $this->excludedFiles = [];
 
-        if ($this->getIgnoredFilesPath() !== []) {
-            $this->excludedFiles = Files::find($dir, $this->getIgnoredFilesPath());
+        if ($exclude !== []) {
+            $this->excludedFiles = Files::find($dir, $exclude);
         }
     }
 
@@ -130,15 +135,5 @@ final class SniffDecorator implements Sniff, Insight, DetailsCarrier, Fixable
             (string) $file->getFileInfo()->getRealPath(),
             $this->excludedFiles
         );
-    }
-
-    /**
-     * Contains the setting for all files which the sniff should ignore.
-     *
-     * @return array<int, string>
-     */
-    private function getIgnoredFilesPath(): array
-    {
-        return $this->sniff->exclude ?? [];
     }
 }
