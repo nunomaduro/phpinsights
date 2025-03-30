77572bb0d3a6fbbd36aa000a619fd5c89b10d3df
Luke Okomilo <github@email.okomilo.com>
2025-03-30 16:28:32 +0100

    chore: fix grammar in "subscribe to youtube" funding messages (#707)

--- src/Application/Console/Commands/InvokableCommand.php
+++ src/Application/Console/Commands/InvokableCommand.php
@@ -17,7 +17,7 @@ final class InvokableCommand extends BaseCommand
         '    <options=bold>https://github.com/nunomaduro/phpinsights</>',
         '  - Sponsor the maintainers:',
         '    <options=bold>https://github.com/sponsors/nunomaduro</>',
-        '  - Subscribe my YouTube channel:',
+        '  - Subscribe to my YouTube channel:',
         '    <options=bold>https://www.youtube.com/@nunomaduro</>',
     ];
 
